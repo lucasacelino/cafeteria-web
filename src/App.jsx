e0126af78b046d9a06ee2f7b11ddb6179f1eae13
lucasacelino <lucasaacelino@gmail.com>
2024-09-26 19:43:31 -0300

    Adicionando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter} from 'react-router-dom';
 import Layout from './templates/Layout';
 import Home from './views/Home';
 import NoPage from './views/NoPage';
 import Login from './views/Login';
 import CardapioYupFormik from './views/CardapioYupFormik';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="cardapio" element={<CardapioYupFormik />} />
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="cardapio" element={<CardapioYupFormik />} />
+//           <Route path="login" element={<Login />} />
+//           <Route path="*" element={<NoPage />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+const router = createBrowserRouter([{
+  path: "/",
+    element: <Layout />,
+    errorElement: <NoPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "cardapio",
+        element: <CardapioYupFormik />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ]
+}])
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+// import App from './App.jsx';
 
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
